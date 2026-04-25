11b3c2de5410cb45a1f4a9e418fccb7d13091a74
uswatun3474 <uswatunhasanah3474@gmail.com>
2026-04-25 17:27:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// AMBIL NAMA TAMU DARI URL
 const urlParams = new URLSearchParams(window.location.search);
 const nama = urlParams.get('to');
 
@@ -5,6 +6,24 @@ if (nama) {
   document.getElementById("namaTamu").innerText = decodeURIComponent(nama);
 }
 
-function playMusic() {
-  document.getElementById("music").play();
+function bukaUndangan() {
+  document.querySelector(".hero").style.display = "none";
+  document.getElementById("content").classList.remove("hidden");
+}
+
+// RSVP
+function kirimRSVP() {
+  const status = document.getElementById("kehadiran").value;
+  const namaTamu = nama || "Tamu";
+
+  saveRSVP(namaTamu, status);
+  alert("RSVP terkirim!");
+}
+
+// UCAPAN
+function kirimUcapan() {
+  const nama = document.getElementById("namaUcapan").value;
+  const isi = document.getElementById("isiUcapan").value;
+
+  saveUcapan(nama, isi);
 }
